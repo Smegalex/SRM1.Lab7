a3eefa95a5ac1b364d96a81140911a0bd55fa548
yoshind <104217441+NaiveeDev@users.noreply.github.com>
2023-12-01 10:05:08 +0200

    Update noda.py

--- src/noda.py
+++ src/noda.py
@@ -1,4 +1,3 @@
-
 ###
 class Node:
     def __init__(self, value):
@@ -6,80 +5,150 @@ class Node:
         self.left = None
         self.right = None
 
+def preorder_traversal(node):
+    if node is not None:
+        return [node.value] + preorder_traversal(node.left) + preorder_traversal(node.right)
+    return []
 
-def inorder_traversal(root):
-    if root:
-        left_str = inorder_traversal(root.left)
-        right_str = inorder_traversal(root.right)
-        return f"{left_str} {root.value} {right_str}" if left_str and right_str else f"{left_str}{root.value}{right_str}"
-    return ""
-
-
-def postorder_traversal(root):
-    if root:
-        left_str = postorder_traversal(root.left)
-        right_str = postorder_traversal(root.right)
-        return f"{left_str} {right_str} {root.value}".strip() if left_str and right_str else str(root.value)
-    return ""
+def postorder_traversal(node):
+    if node is not None:
+        return postorder_traversal(node.left) + postorder_traversal(node.right) + [node.value]
+    return []
 
 
-def build_expression_tree(expression):
-    stack = []
-    current = None
 
-    for char in expression:
-        if char.isalnum():
-            if current is None:
-                current = Node(char)
-            else:
-                current.value += char
-        else:
-            if current is not None:
-                stack.append(current)
-                current = None
+def evaluate_prefix(expression):
+    tokens = expression.split()
+    tokens.reverse()
+    return evaluate_prefix_helper(tokens.copy())
 
-            if char == '(':
-                continue
-            elif char == ')':
-                current = stack.pop()
-                if stack and stack[-1].value in {'+', '-', '*', '/'}:
-                    operator = stack.pop()
-                    operator.right = current
-                    current = operator
-                elif stack and stack[-1].value == '^':
-                    operator = stack.pop()
-                    operator.right = current
-                    current = operator
-                stack.append(current)
-                current = None
-            else:
-                stack.append(Node(char))
 
-    if current is not None:
-        stack.append(current)
+def evaluate_prefix_helper(tokens):
+    if not tokens:
+        return None
 
-    while len(stack) > 1:
-        right = stack.pop()
-        operator = stack.pop()
-        left = stack.pop()
-        operator.left = left
-        operator.right = right
-        stack.append(operator)
+    token = tokens.pop(0)
 
-    return stack[0]
+    if token.isdigit() or (token[0] == '-' and token[1:].isdigit()):
+        return float(token)
+
+    if token in {'+', '-', '*', '/', '^'}:
+        operand1 = evaluate_prefix_helper(tokens.copy())
+        operand2 = evaluate_prefix_helper(tokens.copy())
+
+        if token == '+':
+            return operand1 + operand2
+        elif token == '-':
+            return operand1 - operand2
+        elif token == '*':
+            return operand1 * operand2
+        elif token == '/':
+            return operand1 / operand2
+        elif token == '^':
+            return operand1 ** operand2
+
+
+def evaluate_postfix(expression):
+    tokens = expression.split()
+    return evaluate_postfix_helper(tokens)
+
+
+def evaluate_postfix_helper(tokens):
+    token = tokens.pop(0)
+
+    if token.isdigit() or (token[0] == '-' and token[1:].isdigit()):
+        return float(token)
+
+    if token in {'+', '-', '*', '/', '^'}:
+        operand1 = evaluate_postfix_helper(tokens)
+        operand2 = evaluate_postfix_helper(tokens)
+
+        if token == '+':
+            return operand1 + operand2
+        elif token == '-':
+            return operand1 - operand2
+        elif token == '*':
+            return operand1 * operand2
+        elif token == '/':
+            return operand1 / operand2
+        elif token == '^':
+            return operand1 ** operand2
+
+
+
+
+def printExp(root):
+    print("Прямий обхід:")
+    expression1 = preorder_traversal(root)
+    print(expression1)
+
+    # Зворотній обхід:
+    print("Зворотний обхід:")
+    expression2 = postorder_traversal(root)
+    print(expression2)
+
+    result_prefix = evaluate_prefix(' '.join(map(str, expression1)))
+    result_postfix = evaluate_postfix(' '.join(map(str, expression2)))
+
+    print(f"Результат прямого обходу: {result_prefix}")
+    print(f"Результат зворотнього обходу: {result_postfix}")
+
 
 
 if __name__ == "__main__":
-    equation = "15 ÷ (1 + 2))^(10−2×4)"
 
-    root = build_expression_tree(equation)
+    expression1 = Node('/')
+    expression1.left = Node('+')
+    expression1.right = Node('-')
+    expression1.left.left = Node('6')
+    expression1.left.right = Node('^')
+    expression1.left.right.left = Node('2')
+    expression1.left.right.right = Node('/')
+    expression1.left.right.right.left = Node('9')
+    expression1.left.right.right.right = Node('3')
+    expression1.right.left = Node('11')
+    expression1.right.right = Node('4')
 
-    inorder_result = inorder_traversal(root)
-    inorder_result = " ".join(inorder_result.split())
-    print("Прямий обхід:")
-    print(inorder_result)
+    expression2 = Node('/')
+    expression2.left = Node('+')
+    expression2.right = Node('3')
+    expression2.left.left = Node('2')
+    expression2.left.right = Node('5')
+    expression2.left.left.left = Node('×')
+    expression2.left.left.right = Node('4')
+    expression2.left.left.left.left = Node('+')
+    expression2.left.left.left.right = Node('-')
+    expression2.left.left.left.left.left = Node('7')
 
-    # Зворотній обхід:
-    postorder_result = postorder_traversal(root)
-    print("\nЗворотній обхід:")
-    print(postorder_result)
+    expression3 = Node('×')
+    expression3.left = Node('+')
+    expression3.right = Node('4')
+    expression3.left.left = Node('×')
+    expression3.left.right = Node('3')
+    expression3.left.left.left = Node('-')
+    expression3.left.left.right = Node('2')
+    expression3.left.left.left.left = Node('8')
+    expression3.left.left.left.right = Node('6')
+
+    printExp(expression1)
+    print()
+    printExp(expression2)
+    print()
+    printExp(expression3)
+
+
+    # Варіант 24
+    # (6 + 2^(9÷3)) ÷ (11 − 4)
+    # [["6", "+", ["2", "^", ["9", "/", "3"]]], "/", ["11", "-", "4"]]
+
+    # Варіант 1
+    # ((2+5)×4-7)÷3
+    # [[[["2", "+", "5"], "*", "4"], "-", "7"], "/", "3"]
+
+    # Варіант 19
+    # ((8 − 6) × 2 + 3) × 4
+    # [[[["8", "-", "6"], "*", "2"], "+", "3"], "*", "4"]
+
+    # Варіант 25
+    # 15 ÷ (1 + 2))^(10−2×4)
+    # [["15", "/", ["1", "+", "2"]],"^", ["10", "-", ["2", "*", "4"]]]
